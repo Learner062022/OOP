fbe639ac96e948a9872a32a32b28154240ab2ca7
Learner062022 <ddylan246@yahoo.com>
2026-05-25 19:27:57 +0800

    feat: add Project class

---
+++ DylanDeSouzaOOPPart1/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DylanDeSouzaOOPPart1
+{
+    public class Project : TaskList
+    {
+        public Project(string name) : base(name)
+        {
+            Name = name;
+        }
+
+        public override void AddTask(Task task)
+        {
+            if (task is RepeatingTasks)
+            {
+                Console.WriteLine("Habbits and RepeatingTasks aren't allowed in projects.");
+                return;
+            }
+
+            base.AddTask(task);
+        }
+
+        public float Progress
+        {
+            get
+            {
+                int numTasks = NumTasks;
+                float completedTasks = numTasks - NumIncompleteTasks;
+                return numTasks == 0 ? 0 : (completedTasks / numTasks) * 100;
+            }
+        }
+    }
+}

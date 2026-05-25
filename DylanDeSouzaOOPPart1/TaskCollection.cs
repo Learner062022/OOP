0c4ba6365d41fd762ede05dcce0e04ee5c93de57
Learner062022 <ddylan246@yahoo.com>
2026-05-25 18:17:52 +0800

    feat(task-collection): add TaskCollection class to manage all task lists

---
+++ DylanDeSouzaOOPPart1/TaskCollection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DylanDeSouzaOOPPart1
+{
+    class TaskCollection
+    {
+        List<TaskList> taskLists;
+
+        List<TaskList> TaskLists
+        {
+            set { if (taskLists != value) taskLists = value; }
+            get => taskLists;
+        }
+
+        public int TotalNumTasks
+        {
+            get
+            {
+                int total = 0;
+
+                foreach (TaskList list in taskLists)
+                {
+                    total += list.NumTasks;
+                }
+
+                return total;
+            }
+        }
+
+        public int TotalNumIncompleteTasks
+        {
+            get
+            {
+                int amount = 0;
+
+                foreach (TaskList taskList in taskLists)
+                {
+                    amount += taskList.NumIncompleteTasks;
+                }
+
+                return amount;
+            }
+        }
+
+        public void AddTaskList(TaskList taskList)
+        {
+            taskLists.Add(taskList);
+            save();
+        }
+
+        public void RemoveCompletedTasksPerList()
+        {
+            foreach (TaskList taskList in taskLists)
+            {
+                taskList.RemoveCompletedTasks();
+            }
+        }
+    }
+}

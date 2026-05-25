16b101a08e757e2ec209e1757b547bc89e085bfe
Learner062022 <ddylan246@yahoo.com>
2026-05-25 18:14:17 +0800

    feat(task-list): add TaskList to manage tasks

---
+++ DylanDeSouzaOOPPart1/TaskList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DylanDeSouzaOOPPart1
+{
+    public class TaskList
+    {
+        List<Task> tasks;
+        public string name;
+
+        public TaskList(string name)
+        {
+            tasks = new List<Task>();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Name cannot be empty.");
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+
+        public List<Task> Tasks
+        {
+            get
+            {
+                return tasks;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public int NumTasks
+        {
+            get
+            {
+                return tasks.Count;
+            }
+        }
+
+        public int NumIncompleteTasks
+        {
+            get
+            {
+                int count = 0;
+
+                foreach (Task t in tasks)
+                {
+                    if (!t.IsComplete)
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public virtual void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+        public void RemoveCompletedTasks()
+        {
+            for (int i = tasks.Count - 1; i >= 0; i--)
+            {
+                if (tasks[i].IsComplete)
+                {
+                    tasks.RemoveAt(i);
+                }
+            }
+        }
+    }
+}

ef3dc1acfb22885d7bb05afca18bf9aa5d2f8034
Learner062022 <ddylan246@yahoo.com>
2026-05-25 19:27:18 +0800

    feat(habit): add Habit class

---
+++ DylanDeSouzaOOPPart1/Habit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DylanDeSouzaOOPPart1
+{
+    public class Habit : RepeatingTasks
+    {
+        int completionStreak = 0;
+        DateTime? lastCompleteDate;
+
+        public Habit(
+            string description,
+            bool isComplete,
+            Frequency frequency,
+            string notes = null
+        ) : base(description, isComplete, frequency, notes)
+        {
+        }
+
+        public int CompletionStreak => completionStreak;
+
+        public void UpdateStreak()
+        {
+            if (!RepeatDate.HasValue) return;
+
+            bool isScheduledToday = DateTime.Now.Date == RepeatDate.Value.Date;
+            bool completedOnScheduledDay = IsComplete && isScheduledToday;
+
+            if (!IsComplete && DateTime.Now.Date > RepeatDate.Value.Date)
+            {
+                completionStreak = 0;
+            }
+
+            else if (completedOnScheduledDay && lastCompleteDate != DateTime.Now.Date)
+            {
+                completionStreak++;
+                lastCompleteDate = DateTime.Now.Date;
+            }
+        }
+    }
+}

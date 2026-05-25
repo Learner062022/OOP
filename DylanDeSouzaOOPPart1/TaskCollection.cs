432c1cf8dfbaeb2748673162c7760fcd546cfbbc
Learner062022 <ddylan246@yahoo.com>
2026-05-25 20:53:52 +0800

    feat(storage): add binary file persistence support

--- DylanDeSouzaOOPPart1/TaskCollection.cs
+++ DylanDeSouzaOOPPart1/TaskCollection.cs
@@ -1,10 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Windows.Storage;
+using System.Diagnostics;
 
 namespace DylanDeSouzaOOPPart1
 {
     public class TaskCollection
     {
         List<TaskList> taskLists;
+        StorageFolder storageFolder;
+        StorageFile file;
+        const string FILENAME = "myFile.bin";
+        string filePath;
+
+        public TaskCollection()
+        {
+            taskLists = new List<TaskList>();
+            storageFolder = ApplicationData.Current.LocalFolder;
+            filePath = Path.Combine(storageFolder.Path, FILENAME);
+            load();
+        }
+
+        FileStream OpenFileStream(FileMode mode)
+        {
+            return File.Open(filePath, mode);
+        }
 
         public int TotalNumTasks
         {

3919f6ceac0b9aa967d05bdceae528be6be12211
Learner062022 <ddylan246@yahoo.com>
2026-05-25 19:26:48 +0800

    feat(domain): add RepeatingTasks and Habit

---
+++ TaskManager/Habit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TaskManager
+{
+    public class Habit : RepeatingTasks
+    {
+        int completionStreak = 0;
+        DateTime? lastCompleteDate;
+
+        public Habit(
+            string description,
+            Frequency frequency,
+            string? notes = null
+        ) : base(description, frequency)
+        {
+            Description = description;
+            Notes = notes;
+        }
+
+        public int CompletionStreak => completionStreak;
+
+        public void UpdateStreak()
+        {
+            if (!RepeatDate.HasValue) return;
+
+            bool isScheduledToday = DateTime.Now.Date == RepeatDate.Value.Date;
+            bool completedOnScheduledDay = IsComplete && isScheduledToday;
+
+            if (!IsComplete && DateTime.Now.Date > RepeatDate.Value.Date)
+            {
+                completionStreak = 0;
+            }
+
+            else if (completedOnScheduledDay && lastCompleteDate != DateTime.Now.Date)
+            {
+                completionStreak++;
+                lastCompleteDate = DateTime.Now.Date;
+            }
+        }
+    }
+}
---
+++ TaskManager/RepeatingTasks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TaskManager
+{
+    public class RepeatingTasks : Task
+    {
+        Frequency frequency;
+        DateTime? repeatDate;
+
+        public enum Frequency
+        {
+            Weekly = 7,
+            Daily = 1
+        }
+
+        public RepeatingTasks(
+            string description,
+            Frequency frequency
+        ) : base(description)
+        {
+            this.frequency = frequency;
+        }
+
+        public override void ToggleCompleteStatus()
+        {
+            base.ToggleCompleteStatus();
+
+            if (IsComplete)
+            {
+                repeatDate = DateTime.Now.Date.AddDays((int)frequency);
+            }
+        }
+
+        public DateTime? RepeatDate => repeatDate;
+    }
+}

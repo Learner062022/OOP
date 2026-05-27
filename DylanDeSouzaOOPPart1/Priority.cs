d6b1b5178562a016832af79b75d129589e8ec4b4
Learner062022 <ddylan246@yahoo.com>
2026-05-27 11:25:32 +0800

    refactor(task): extract Priority type into separate component

---
+++ DylanDeSouzaOOPPart1/Priority.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DylanDeSouzaOOPPart1
+{
+    public struct Priority
+    {
+        int value;
+
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public Priority(int v)
+        {
+            value = Math.Clamp(v, -1, 1);
+        }
+
+        public static Priority operator ++(Priority p)
+        {
+            if (p.value < 1)
+            {
+                p.value++;
+            }
+                
+            return p;
+        }
+
+        public static Priority operator --(Priority p)
+        {
+            if (p.value > -1)
+            {
+                p.value--;
+            }
+                
+            return p;
+        }
+    }
+}

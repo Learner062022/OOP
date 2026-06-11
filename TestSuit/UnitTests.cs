7b96143983bf1a105797baef6c41c0f25445d1b1
Learner062022 <ddylan246@yahoo.com>
2026-06-11 11:35:29 +0800

    test: complete AddListToCollection test

--- TestSuit/UnitTests.cs
+++ TestSuit/UnitTests.cs
@@ -34,6 +34,9 @@ namespace TestSuit
         [TestMethod]
         public void AddListToCollection()
         {
+            list.AddTask(task);
+            collection.AddTaskList(list);
+            Assert.AreEqual(1, collection.TotalNumTasks);
         }
 
         [TestMethod]

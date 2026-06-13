f63807e76391a4472dde8bc6396df7fa397d9db7
Learner062022 <ddylan246@yahoo.com>
2026-06-13 18:05:26 +0800

    feat: add a list serializer

---
+++ TaskManager/TaskListSerializer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TaskManager
+{
+    public class TaskListSerializer
+    {
+        TaskSerializer taskSerializer;
+
+        public TaskListSerializer(TaskSerializer serializer)
+        {
+            taskSerializer = serializer;
+        }
+
+        public void WriteTaskList(BinaryWriter writer, TaskList list)
+        {
+            writer.Write(list.GetType().Name);
+            writer.Write(list.Name);
+            writer.Write(list.NumTasks);
+
+            foreach (var task in list.Tasks)
+            {
+                taskSerializer.WriteTask(writer, task);
+            }
+        }
+
+        public TaskList ReadTaskList(BinaryReader reader)
+        {
+            string taskListType = reader.ReadString();
+            string name = reader.ReadString();
+
+            TaskList list;
+
+            switch (taskListType)
+            {
+                case "Project":
+                    list = new Project(name);
+                    break;
+
+                default:
+                    list = new TaskList(name);
+                    break;
+            }
+
+            int numTasks = reader.ReadInt32();
+
+            while (numTasks > 0)
+            {
+                Task task = taskSerializer.ReadTask(reader);
+                list.AddTask(task);
+                numTasks--;
+            }
+
+            return list;
+        }
+    }
+}

5bf74a128e10b4567aa233da845fbd3f529c9e4e
Learner062022 <ddylan246@yahoo.com>
2026-06-16 14:54:51 +0800

    feat(domain): add RepeatingTasks and Habit

---
+++ TaskManager/RepeatingTasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TaskManager
+{
+    public class RepeatingTasks : Task
+    {
+        Frequency frequency;
+        DateTime? repeatDate;
+
+        public enum Frequency
+        {
+            Weekly = 7,
+            Daily = 1
+        }
+
+        public RepeatingTasks(
+            string description,
+            bool isComplete,
+            Frequency frequency,
+            string notes = null
+        ) : base(description, notes)
+        {
+            this.frequency = frequency;
+        }
+
+        public override void ToggleCompleteStatus()
+        {
+            base.ToggleCompleteStatus();
+
+            if (IsComplete)
+            {
+                repeatDate = DateTime.Now.Date.AddDays((int)frequency);
+            }
+        }
+
+        public DateTime? RepeatDate => repeatDate;
+    }
+}

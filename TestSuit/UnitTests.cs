928339138a72f7d58d3488a2dc16f25916529f6d
Learner062022 <ddylan246@yahoo.com>
2026-06-16 14:55:10 +0800

    test: add fixtures and complete core behavior tests

--- TestSuit/UnitTests.cs
+++ TestSuit/UnitTests.cs
@@ -8,19 +8,25 @@ namespace TestSuit
     {
         TaskCollection collection;
         TaskList list;
-
+        Task task;
+        Project project;
+        RepeatingTasks repeatingTask;
+        Habit habit;
 
         [TestInitialize]
         public void Setup()
         {
             collection = new TaskCollection();
-            list = new TaskList("taskList1");
+            list = new TaskList("list1");
+            task = new Task("task1");
+            project = new Project("project1");
+            repeatingTask = new RepeatingTasks("repeatingTask1", RepeatingTasks.Frequency.Daily);
+            habit = new Habit("habit1", Habit.Frequency.Weekly);
         }
 
         [TestMethod]
         public void AddingTasksAndCount()
         {
-            Task task = new Task("task1");
             list.AddTask(task);
             Assert.AreEqual(1, list.NumTasks);
         }
@@ -28,16 +34,23 @@ namespace TestSuit
         [TestMethod]
         public void AddListToCollection()
         {
+            list.AddTask(task);
+            collection.AddTaskList(list);
+            Assert.AreEqual(1, collection.TotalNumTasks);
         }
 
         [TestMethod]
-        public void SetTaskNameBlank()
+        public void SetTaskDescriptionBlank()
         {
+            task.Description = "";
+            Assert.AreEqual("task1", task.Description);
         }
 
         [TestMethod]
         public void SetListNameBlank()
         {
+            list.Name = "";
+            Assert.AreEqual("list1", list.Name);
         }
 
         [TestMethod]
@@ -48,11 +61,16 @@ namespace TestSuit
         [TestMethod]
         public void ProjectPercentComplete()
         {
+            task.ToggleCompleteStatus();
+            project.AddTask(task);
+            Assert.AreEqual(100, project.Progress);
         }
 
         [TestMethod]
         public void PlaceHabitInProject()
         {
+            project.AddTask(habit);
+            Assert.AreEqual(0, project.NumTasks);
         }
 
         [TestMethod]
@@ -63,21 +81,29 @@ namespace TestSuit
         [TestMethod]
         public void PlaceRepeatingTaskInProject()
         {
+            project.AddTask(repeatingTask);
+            Assert.AreEqual(0, project.NumTasks);
         }
 
         [TestMethod]
         public void CompleteIncompleteTaskUpdatesCount()
         {
+            list.AddTask(task);
+            task.ToggleCompleteStatus();
+            Assert.IsTrue(task.IsComplete);
+            Assert.AreEqual(0, list.NumIncompleteTasks);
+            task.ToggleCompleteStatus();
+            Assert.IsFalse(task.IsComplete);
+            Assert.AreEqual(1, list.NumIncompleteTasks);
         }
 
         [TestMethod]
         public void DeleteTasksUpdatesCount()
         {
-        }
-
-        [TestMethod]
-        public void EmptyListUpdatesCount()
-        {
+            list.AddTask(task);
+            task.ToggleCompleteStatus();
+            list.RemoveCompletedTasks();
+            Assert.AreEqual(0, list.NumTasks);
         }
     }
 }
